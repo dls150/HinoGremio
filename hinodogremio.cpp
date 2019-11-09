d83e7afcbdd35d4b92f7407cd6d2107acc113d5d
dls150 <150105@upf.br>
2019-11-08 22:24:21 -0300

    Fonte em C++

---
+++ hinodogremio.cpp
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <iostream>
+#include <stdlib.h>
+#include <locale.h>
+#define T 250
+using namespace std;
+
+int main() {
+setlocale(LC_ALL,"Portuguese");
+
+
+                              cout<< "\nAté a pé nós iremos,...";
+                            	Beep(550,T);
+                            	Beep(495,T);
+                            	Beep(495,2*T);
+                            	Beep(495,T);
+                            	Beep(550,T);
+                            	Beep(495,2*T);
+                            	Beep(330,6*T);
+
+                                cout<< " para o que der e vier.";
+                            	Beep(660,2*T);
+                            	Beep(550,T);
+                            	Beep(495,T);
+                            	Beep(440,2*T);
+                            	Beep(495,T);
+                            	Beep(550,T);
+                            	Beep(495,6*T);
+
+                            	cout<< "\nMas o certo, ‚ que nós estaremos...";
+                            	Beep(550,T);
+                            	Beep(594,T);
+                            	Beep(660,2*T);
+                            	Beep(634,T);
+                            	Beep(660,T);
+                            	Beep(704,2*T);
+                            	Beep(660,T);
+                            	Beep(704,T);
+                            	Beep(844,2*T);
+                            	Beep(734,4*T);
+
+                            	cout<< "com o Grêmio, onde o Grêmio estiver!";
+                            	Beep(550,T);
+                            	Beep(594,T);
+                            	Beep(660,2*T);
+                            	Beep(734,T);
+                            	Beep(660,T);
+                            	Beep(495,2*T);
+                            	Beep(594,T);
+                            	Beep(550,T);
+                            	Beep(440,4*T);
+
+}
